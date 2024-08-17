df0a905035fa0d98d0cd9b8ae55e3c946b9ca28b
durairam-dev <durairam1221@gmail.com>
2024-08-18 01:48:47 +0530

    Reset all files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
       <h1>App</h1>
     </>
   );
-}
+};
 
 export default App;
